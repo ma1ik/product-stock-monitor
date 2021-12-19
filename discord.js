0a670b1786802ddf03112514002f5d31d3b08b99
Abdulmalik <hazardgamin9@gmail.com>
2021-12-19 23:00:52 +0000

    Created discord.js to allow bot to join and come online

---
+++ discord.js
@@ -0,0 +1,26 @@
+/*
+This is for the discord server, where the bot joins the server and comes online using the client, token
+You can see it here: https://discordjs.guide/creating-your-bot/command-handling.html#individual-command-files
+ */
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'beep') {
+        await interaction.reply('Boop!');
+    }
+});
+
+client.login(token);
