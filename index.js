96e0c371ef5a14e37760a92c15d2defbeddb4ab4
Abdulmalik <hazardgamin9@gmail.com>
2021-12-20 00:25:18 +0000

    Created a GET request to pull the website page source

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // Main file for creating the monitor to function
 
+const https = require('https');
+
+https.get('https://www.simplygames.com/', (response) => {
+    let data = '';
+    response.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    response.on('end', () => {
+        console.log(data);
+    });
+})
+
+.on('error', (error) => {
+    console.log(error);
+});
