ab9798e4fc8d082f36efe59581a8b012e89d07af
Abdulmalik <hazardgamin9@gmail.com>
2021-12-24 20:00:02 +0000

    Using node-fetch now for the library in node

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-import got from "got"
+// Time to develop this product stock monitor in node-fetch module only
+// Lets do some today, Christmas day will be my off day, so let's get cracking!
 
 
-got.get('https://jsonplaceholder.typicode.com/users', {responseType: 'json'})
-    .then(res => {
-        const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-        console.log('Status Code:', res.statusCode);
-        console.log('Date in Response header:', headerDate);
-
-        const users = res.body;
-        for(let user of users) {
-            console.log(`Got user with id: ${user.id}, name: ${user.name}`);
-        }
-    })
-    .catch(err => {
-        console.log('Error: ', err.message);
-    });
