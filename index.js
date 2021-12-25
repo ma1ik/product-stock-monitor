750e1231e66a1297a35a3b2809d729d583fac932
Abdulmalik <hazardgamin9@gmail.com>
2021-12-25 20:41:26 +0000

    updated url for simplygames as before it was a test url

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 
 // Sending a Get request to the URL - SimplyGames
 const get_request = async () => {
-    const response = await fetch('https://github.com/');
+    const response = await fetch('https://www.simplygames.com/');
     const body = await response.text();
     console.log(body);
 
