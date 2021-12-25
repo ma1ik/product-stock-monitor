81bc928b3f6714173393203e2640a2d1c9606ebd
Abdulmalik <hazardgamin9@gmail.com>
2021-12-25 22:43:09 +0000

    Fixed the headers const not working - coming up with an error.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Import the fetch api for node
-import fetch from 'node-fetch';
+import fetch, {Headers} from 'node-fetch';
+
+// Added
+const meta = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62',
+};
+const headers = new Headers(meta);
 
 // Sending a Get request to the URL - SimplyGames
 const get_request = async () => {
-    const response = await fetch('https://www.simplygames.com/');
+    const response = await fetch('https://www.simplygames.com/',
+        {headers});
     const body = await response.text();
     console.log(body);
 
