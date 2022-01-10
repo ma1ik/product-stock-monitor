0eab2e86eef643bac6d488966819007c0a44cd1c
Abdulmalik <hazardgamin9@gmail.com>
2022-01-10 23:05:50 +0000

    Web scraped some code to show the price, product name and stock status

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // Import axios for requests module & cheerio for web scraping
 import axios from 'axios';
 import cheerio from "cheerio";
-// Look at Google Notes - continue where I left from.
+
 // https://www.simplygames.com/p/xbox-series-s-fortnite-and-rocket-league-bundle-xbox-series-x--s
+
+// Getting the product url and replacing it with a product ID
 const getProductUrl = (product_id) => `https://www.simplygames.com/p/${product_id}`;
 
+// Function for getting the product ID information with the use of headers to allow us to act like a web browser & avoid bot detection.
 async function getProduct(product_id) {
     const productUrl = getProductUrl(product_id);
     const { data }= await axios.get(productUrl, {
@@ -14,57 +17,20 @@ async function getProduct(product_id) {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.55',
             'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'en-GB,en;q=0.9,en-US;q=0.8',
-
-        },
+        }
     });
-    console.log(data);
+
+    const $ = cheerio.load(data);
+    const product = $('.product_content').children('h1').text();
+    const stockStatus = $('.in_stock').text();
+    const price = $('.price_point').text();
+    const productInfo = {
+        'Product Name': product,
+        'Stock Status': stockStatus,
+        'Price': price,
+    }
+    console.log(productInfo);
 }
 
 getProduct('xbox-series-s-fortnite-and-rocket-league-bundle-xbox-series-x--s');
-
-
-/*
-// Sending a Get request to the URL - SimplyGames
-const get_request = async () => {
-    const response = await fetch(url,
-        {headers});
-    const body = await response.text();
-    const $ = cheerio.load(body);
-    const product = $('.product_content').children('h1').text();
-    const stockStatus = $('.in_stock').text().trim();
-    const price = $('.price_point').text();
-
-    const productInfo = {
-        "Product name": product,
-        "Stock Status": stockStatus,
-        "Price": price
-    };
-
-
-    console.log(productInfo);
-
-};
-
-// Runs the get_request function
-get_request();
-*/
-
-
-
-// TODO
-/*
-First task to do
-
-- Make it so each product prints out the following:
-- Title
-- Stockstatus aka true or false
-- Price
-- Image can be later for discord
-Unit test regularly for each function
-
-Do this for all products that I want monitor, may want to put it in an array
-I can have an empty array which has all the products it wants to monitor // a file it
-wants to read from
- */
-
 
