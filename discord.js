652e99f426be7b2227c25c2fb6da366daf866da9
Abdulmalik <hazardgamin9@gmail.com>
2022-02-05 21:36:59 +0000

    created client instance for discord.js

--- discord.js
+++ discord.js
@@ -1,27 +1,14 @@
-/*
-You'll need to update the discord.js version from v12 to v13 - see website
-This is for the discord server, where the bot joins the server and comes online using the client, token
-You can see it here: https://discordjs.guide/creating-your-bot/command-handling.html#individual-command-files
- */
+// Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
+// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+// When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const { commandName } = interaction;
-
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    } else if (commandName === 'beep') {
-        await interaction.reply('Boop!');
-    }
-});
-
+// Login to Discord with your client's token
 client.login(token);
