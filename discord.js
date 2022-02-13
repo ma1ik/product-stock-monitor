3a2c2d44ef8c9ba6388f2250ccce1f185ec1d33e
Abdulmalik <hazardgamin9@gmail.com>
2022-02-13 22:21:31 +0000

    added an array with random responses

--- discord.js
+++ discord.js
@@ -10,6 +10,9 @@ client.once('ready', () => {
     console.log(`${client.user.tag} has logged in.`);
 });
 
+
+//const greetings = ['Hello', 'Hey', 'Hi' ,'Whats up', "what's up?", 'whats up?'];
+
 // When user sends message in Discord, message appears in terminal
 client.on('messageCreate', message => {
     // checks if author who sent message is a bot
@@ -17,7 +20,11 @@ client.on('messageCreate', message => {
     console.log(message.content);
     // compares message with user and bot replies
     if (message.content === 'Hello') {
-        message.channel.send('Hey there!');
+        // randomly picks a response to reply to user
+        let greetings = ['Hi', 'Hey', 'Hello', 'yo', "what's up?"];
+        let randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+        message.channel.send(randomGreeting);
     }
 });
 
