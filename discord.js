f8477b354a81b214c6d8f9d4ac5f3bc6a75f1232
Abdulmalik <hazardgamin9@gmail.com>
2022-03-07 00:31:12 +0000

    working on else if

--- discord.js
+++ discord.js
@@ -11,27 +11,29 @@ client.once('ready', () => {
 });
 
 // When user sends message in Discord, message appears in terminal
-client.on('messageCreate', message => {
+client.on('messageCreate',  message => {
     // checks if author who sent message is a bot
     if (message.author.bot) return;
     console.log(message.content);
     let greetings = ['hi', 'hey', 'sup', 'hello'];
-    let trackStock = ['i would like to track stock', 'track stock'];
+    let trackStock = ['i would like to track stock', 'track stock', 'i want to track stock'];
+
     // message accepts any greeting from the above array and bot will reply
     if (greetings.includes(message.content.toLowerCase())) {
         // randomly picks a response to reply to user
         let randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
         // greets user's name and asks how can I help
         message.channel.send(randomGreeting + " " + message.author.username + ", how can I help you today?");
-        message.channel.send('Please select from the options below:' + '\n' +
-            'I would like to track stock')
+
+
         // else statement if user doesn't put a valid input
-    } else {
-        message.channel.send('Sorry, I didn\'t quite understand that, can you make sure the input is correct!');
+    } else if (!(greetings) || (!trackStock)){
+      message.channel.send('Sorry, I didn\'t quite understand that, can you make sure the input is correct!');
     }
+
     // User asks to track stock
    if (trackStock.includes(message.content.toLowerCase())) {
-        message.channel.send('Alright, what product would you like to track?')
+       message.channel.send('Alright, what product would you like to track?')
     }
 });
 
