672a36b86692e6aec3521c6b1bf33dcb8372b85d
Abdulmalik <hazardgamin9@gmail.com>
2022-03-14 00:00:58 +0000

    Added messageCollector

--- interaction.js
+++ interaction.js
@@ -1,8 +1,6 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
-
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 // When the client is ready, run this code (only once)
@@ -10,61 +8,31 @@ client.once('ready', () => {
     console.log(`${client.user.tag} has logged in.`);
 });
 
-client.login(token);
+client.on('messageCreate',  (message) => {
 
-module.exports = {
+    if (message.author.bot) return;
 
-    callback: ({ message, channel}) => {
-        message.reply('Enter your name:')
 
-        const filter = (m) => {
-            return m.author.id === m.author.id;
-        }
+    if (message.content === "?hello") {
+        message.channel.send("Hello there, What is your name?");
+        // `m` is a message object that will be passed through the filter function
+        const filter = m => m.content.includes('discord');
+        const collector = message.channel.createMessageCollector({ filter, time: 15000 });
 
-        const collector = channel.createMessageCollector({
-            filter,
-            max: 1,
-            time: 5000
-        })
+    // Code here
 
-        collector.on('collect', message => {
-            console.log(message.content)
-        })
+        collector.on('collect', m => {
+            console.log(`Collected ${m.content}`);
+        });
 
         collector.on('end', collected => {
-            if (collected.size === 0) {
-                message.reply('You did not provide your name')
-                return
-            }
-
-            let text = 'Collected:\n\n'
-
-            collected.forEach((message) => {
-                text += `${message.content}\n`
-            })
-
-            message.reply(text);
-
-        })
+            console.log(`Collected ${collected.size} items`);
+        });
 
     }
+});
 
-}
 
-client.on('messageCreate', async (message) => {
-        if (message.author.bot) return;
 
-        const filter = (m) =>
-            m.author.id === message.author.id;
 
-        if (message.content === "?hello") {
-            message.channel.send("Hello there, What is your name?");
-
-            const msgs = await message.channel.awaitMessages(filter, {time: 5000});
-            console.log(msgs.size);
-
-        }
-    });
-
-// Login to Discord with your client's token
-
+client.login(token);
