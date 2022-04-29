2d56150f57e71f3ff83c1bc0300cbbe732d18631
Abdulmalik <hazardgamin9@gmail.com>
2022-04-29 11:29:03 +0100

    removed some imports

--- interaction.js
+++ interaction.js
@@ -2,7 +2,7 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-let { getProductZavvi, zavvi, arraySelection } = require("./index.js");
+let { zavvi } = require("./index.js");
 
 client.login(token)
 
